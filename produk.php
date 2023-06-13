48a2543b41fa2c1ff4dd391fd51b8b2d09de8e8e
vaylerie <valleriebritania@gmail.com>
2023-06-13 19:22:17 +0800

    fix bug pop-up modal

--- produk.php
+++ produk.php
@@ -70,7 +70,7 @@ include('config.php'); // file koneksi ke database
                     // Konversi gambar dari byte menjadi data URL
                     $gambar = 'data:image/jpeg;base64,' . base64_encode($gambarData);
             ?>
-                    <div class="w-full m-1 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800    dark:border-gray-700 basis-1/5">
+                    <div class="w-full m-1 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 basis-1/5">
                         <div class="flex grid grid-cols-1">
                             <a href="#">
                                 <img class="p-8 rounded-t-lg" src="<?php echo $gambar; ?>" alt="product image" />
@@ -79,12 +79,12 @@ include('config.php'); // file koneksi ke database
                         <div class="px-5 pb-5">
 
                             <!-- Modal toggle -->
-                            <a data-modal-target="staticModal" data-modal-toggle="staticModal" class="block hover:underline font-semibold tracking-tight text-gray-900 rounded-lg text-l" type="button">
-                                <?php echo $nama_obat ?>
+                            <a data-modal-target="<?php echo 'staticModal-' . $kd_obat; ?>" data-modal-toggle="<?php echo 'staticModal-' . $kd_obat; ?>" class="block hover:underline font-semibold tracking-tight text-gray-900 rounded-lg text-l" type="button">
+                                <?php echo $nama_obat; ?>
                             </a>
 
                             <!-- Main modal -->
-                            <div id="staticModal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                            <div id="<?php echo 'staticModal-' . $kd_obat; ?>" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
                                 <div class="relative w-full max-w-2xl max-h-full">
                                     <!-- Modal content -->
                                     <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -93,7 +93,7 @@ include('config.php'); // file koneksi ke database
                                             <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
                                                 <?php echo $nama_obat ?>
                                             </h3>
-                                            <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="staticModal">
+                                            <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="<?php echo 'staticModal-' . $kd_obat; ?>">
                                                 <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                     <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                                                 </svg>
@@ -122,14 +122,14 @@ include('config.php'); // file koneksi ke database
                             </div>
                             <div class="flex grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-4 lg:items-center lg:justify-between md:justify-normal mx-auto">
                                 <span class="text-l font-bold text-gray-900 dark:text-white">Rp. <?php echo $harga; ?></span>
-                                <button data-modal-target="popup-modal" data-modal-toggle="popup-modal" class="block text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm m-3 px-2.5 py-2 text-center" type="button">
+                                <button data-modal-target="<?php echo 'popup-modal-' . $kd_obat; ?>" data-modal-toggle="<?php echo 'popup-modal-' . $kd_obat; ?>" class="block text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm m-3 px-2.5 py-2 text-center" type="button">
                                     Beli
                                 </button>
 
-                                <div id="popup-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                                <div id="<?php echo 'popup-modal-' . $kd_obat; ?>" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
                                     <div class="relative w-full max-w-md max-h-full">
                                         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                                            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="popup-modal">
+                                            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="<?php echo 'popup-modal-' . $kd_obat; ?>">
                                                 <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                     <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                                                 </svg>
@@ -140,10 +140,10 @@ include('config.php'); // file koneksi ke database
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                                                 </svg>
                                                 <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Anda harus login dahulu untuk melakukan transaksi</h3>
-                                                <a data-modal-hide="popup-modal" type="button" class="text-white bg-green-600 hover:bg-green-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2" href="login.php">
+                                                <a data-modal-hide="<?php echo 'popup-modal-' . $kd_obat; ?>" type="button" class="text-white bg-green-600 hover:bg-green-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2" href="login.php">
                                                     Login
                                                 </a>
-                                                <button data-modal-hide="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 text-center">Batal</button>
+                                                <button data-modal-hide="<?php echo 'popup-modal-' . $kd_obat; ?>" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 text-center">Batal</button>
                                             </div>
                                         </div>
                                     </div>
@@ -165,6 +165,7 @@ include('config.php'); // file koneksi ke database
         </div>
 
     </section>
+
 
     <footer class="bg-slate-50 dark:bg-gray-900">
         <div class="mx-auto w-full max-w-screen-xl p-20 py-6 lg:py-8">
